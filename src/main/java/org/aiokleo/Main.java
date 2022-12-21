8ea57877c554388ed39cf03d4a6f52d7414a8679
Amir-UL <mangoleo51@gmail.com>
2022-12-21 17:26:03 +0600

    JDBC is Done in markdown format

---
+++ src/main/java/org/aiokleo/Main.java
@@ -0,0 +1,11 @@
+package org.aiokleo;
+
+import org.aiokleo.appUser.AppUser;
+
+public class Main {
+    public static void main(String[] args) {
+        AppUser appUser = new AppUser();
+
+    }
+
+}
---
+++ src/main/java/org/aiokleo/appUser/AppUser.java
@@ -0,0 +1,41 @@
+package org.aiokleo.appUser;
+
+public class AppUser { // Also called POJO/BEAN
+    private int id;
+    private String name;
+    private String email;
+    private String date_of_birth;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(String date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+}
